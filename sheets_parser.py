503fc4bc6dcee545798bc7a916fde945cf009418
Deskent <battenetciz@gmail.com>
2022-09-26 19:55:11 +0300

    add sheet parsing

---
+++ sheets_parser.py
@@ -0,0 +1,113 @@
+from datetime import date
+from decimal import Decimal
+
+import gspread
+from gspread import Worksheet, Spreadsheet, Client
+import requests
+import xmltodict
+
+
+class SheetsParser:
+    """
+    Parse data from Google Sheet. Get exchange rate from CBR.
+    Update parsed data with exchange rate for every record.
+
+    Attributes
+
+        currency: str
+            Currency for getting exchange rate
+
+        sheet_url: str
+            URL to Google Sheet
+
+        google_token_filename: str = 'token.json'
+            Filepath to file with Google Sheet access token
+
+    Methods
+        get_data
+        get_google_datalist
+        get_exchange_rate
+    """
+
+    def __init__(self, currency: str, sheet_url: str, google_token_filename: str = 'token.json'):
+        self.currency: str = currency
+        self.sheet_url: str = sheet_url
+        self.google_token_filename: str = google_token_filename
+
+    def get_data(self) -> list[dict]:
+        """Returns parsed data from Google Sheets with adding
+        current exchange rate for every record
+
+        :return: Updated data list[dict]
+        """
+
+        google_data: list[dict] = self.get_google_datalist()
+        exchange_rate: Decimal = self.get_exchange_rate()
+        updated_data: list[dict] = self.__update_with_exchange_rate(google_data, exchange_rate)
+
+        return updated_data
+
+    def get_google_datalist(self) -> list[dict]:
+        """
+        Returns parsed data from google sheet
+
+        :return: Parsed data list[dict]
+        """
+
+        client: Client = gspread.service_account(filename=self.google_token_filename)
+
+        sheets: Spreadsheet = client.open_by_url(self.sheet_url)
+        worksheet: Worksheet = sheets.get_worksheet(0)
+        worksheet_data: list[dict] = worksheet.get_all_records()
+
+        return worksheet_data
+
+    def get_exchange_rate(self, currency: str = '') -> Decimal:
+        """
+        Returns current currency value from CBR API
+
+        :return: (Decimal) Rounded value
+        """
+
+        if not currency:
+            currency = self.currency
+        current_date: str = date.today().strftime("%d/%m/%Y")
+        url = f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}'
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise requests.RequestException(f'Request error: status code: {response.status_code}')
+        answer: str = response.text
+        if currency in answer:
+            parsed_data: dict[str, dict] = xmltodict.parse(answer)
+            data: list[dict] = parsed_data['ValCurs']['Valute']
+            for elem in data:
+                if elem['CharCode'] == currency:
+                    value: str = elem.get('Value').replace(',', '.')
+
+                    return Decimal(value).quantize(Decimal('1.00'))
+
+        raise ValueError("Currency not found")
+
+    @staticmethod
+    def __update_with_exchange_rate(
+            data: list[dict],
+            exchange_rate: Decimal,
+            name: str = 'стоимость,руб'
+    ) -> list[dict]:
+
+        """Returns updated list of dictionaries
+        with current exchange rate for every element
+
+        :return: Updated list[dict]
+        """
+
+        result = data[:]
+        for elem in result:
+            usd_price: int = int(elem['стоимость,$'])
+            rubles_price: Decimal = usd_price * exchange_rate
+            elem[name] = rubles_price.to_eng_string()
+
+        return result
+
+
+
